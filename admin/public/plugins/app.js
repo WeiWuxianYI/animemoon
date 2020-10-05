b1aab99b93e1d965b459e3efb8feaeebeec39bdd
Wei <70841417+WeiWuxianYI@users.noreply.github.com>
2020-10-05 10:53:33 +0200

    Add files via upload

--- admin/public/plugins/app.js
+++ admin/public/plugins/app.js
@@ -1,1 +1,11 @@
-
+function openModal(id) {
+    let modal = $(id);
+    let cont = modal.find('.modal-content');
+    cont.empty();
+    cont.append('<img src="public/images/loading.svg" style="width: 40px;display: block;margin: 0 auto" class="rotating">');
+    modal.addClass('active');
+}
+function closeModal(id) {
+    let modal = $(id);
+    modal.removeClass('active');
+}
